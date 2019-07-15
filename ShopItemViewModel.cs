89ab81624f10a42b9e8aaa08b91a28d0da9966ac
Carla17053422 <52859408+Carla17053422@users.noreply.github.com>
2019-07-15 08:31:05 +0200

    Add files via upload

---
+++ ShopItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW_Assignment_07.Views.Shop
+{
+    public class ShopItemViewModel
+    {
+        private string mName;
+        private string mDescription;
+        private double mPrice;
+        private int mQuantityAvailable;
+
+        public string Name { get => mName; set => mName = value; }
+        public string Description { get => mDescription; set => mDescription = value; }
+        public double Price { get => mPrice; set => mPrice = value; }
+        public int GuantityAvailable { get => mQuantityAvailable; set => mQuantityAvailable = value; }
+        
+        //add an empty constructor method
+    }
+}
